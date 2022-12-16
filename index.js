f9822c2e4daf0d90e15b773f9ec9c08e5060e189
mkmach <mkmach@hotmail.com>
2022-12-16 11:58:57 -0800

    iss with promises

--- index.js
+++ index.js
@@ -28,25 +28,6 @@ fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   console.log('It worked! Returned flyover times:' , passTimes);
 });
 
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  // success, print out the deets!
-  console.log(passTimes);
-});
-
-/** 
- * Input: 
- *   Array of data objects defining the next fly-overs of the ISS.
- *   [ { risetime: <number>, duration: <number> }, ... ]
- * Returns: 
- *   undefined
- * Sideffect: 
- *   Console log messages to make that data more human readable.
- *   Example output:
- *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
- */
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
@@ -60,9 +41,9 @@ nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  // success, print out the deets!
   printPassTimes(passTimes);
 });
+
 
 module.exports = { 
   fetchMyIP,
